c9ebb4eeddc149caee4d73d7d1a48297d4e2dd3f
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-10-25 16:06:34 +0000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  
+
+    
+    get("/square/results", {:controller=> "new", :action => "square"})
+    get("/root/results" {:controller=> "new", :action => "square_root"})
+    get("/payment/results", {:controller=> "new", :action => "payment"})
+    get("/random/results", {:controller=> "new", :action => "random_num"})
+    get("/", {:controller => "new", :action => "homepage"})
+
 end
